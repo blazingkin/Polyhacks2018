2cbd5553c11895241e081503ea413436724df52b
Alex Gravenor <blazingkin@gmail.com>
2018-01-13 15:24:33 -0800

    Fix bug

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -8,7 +8,7 @@ class Parent < ApplicationRecord
     #                format: { with: VALID_EMAIL_REGEX }
 
     def self.from_omniauth(auth)
-        where(auth.permit(:provider, :uid)).first_or_initialize.tap do |user|
+        where(auth.permit(:uid)).first_or_initialize.tap do |user|
           user.provider = auth.provider
           user.uid = auth.uid
           user.name = auth.info.name
