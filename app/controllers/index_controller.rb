a340ff047b1ef21348afc411facd037c438242d5
Alex Gravenor <blazingkin@gmail.com>
2018-01-13 11:46:03 -0800

    Listing pages

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,10 @@
 class IndexController < ApplicationController
 
     def index
+        unless @current_user.nil?
+            @jobs = JobListing.all
+            @jobs = @jobs.page(params[:page])
+        end
     end
 
 end
